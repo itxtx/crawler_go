a122fbceedda5574160ff5b534ae5426df07f9fd
itxtx <68209227+itxtx@users.noreply.github.com>
2024-08-26 11:43:57 +0200

    add to main

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Check the number of arguments
+	if len(os.Args) < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	} else if len(os.Args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	} else {
+		// Get the base URL from the command line arguments
+		baseURL := os.Args[1]
+		fmt.Printf("starting crawl of: %s\n", baseURL)
+		// Here you can call your crawler function
+		// crawl(baseURL)
+	}
 }
